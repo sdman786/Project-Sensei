36580481a1b28ab666fbc315035c42c198d9418b
Salman Dadabhoy <dadabhoysalman@outlook.com>
2019-04-04 15:38:54 +0100

    quiz service port changed

--- frontend/src/app/services/quiz.service.ts
+++ frontend/src/app/services/quiz.service.ts
@@ -4,22 +4,12 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
 export class QuizService {
-  baseUrl:string = "http://localhost:3000";
+  baseUrl:string = "http://localhost:8080";
   constructor(private http: HttpClient) { }
 
-  get(url: string) {
-    return this.http.get(url);
-  }
-
-  getAll() {
-    return [
-      { id: 'data/agile.json', name: 'Agile Quiz' },
-      { id: 'data/scrum.json', name: 'Scrum Quiz' }
-    ];
-  }
-
-  get_AgileQuiz() {
-    return this.http.get(this.baseUrl + '/agile');
+  get_MCQ(mcqName: string) {
+    return this.http.get(`http://localhost:8080/${mcqName}`);
   }
 }
