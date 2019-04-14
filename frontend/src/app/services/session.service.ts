603a032229c256a057f7b8a44102152b68b8f93b
Salman Dadabhoy <dadabhoysalman@outlook.com>
2019-04-14 04:49:19 +0100

    json server refined

--- frontend/src/app/services/session.service.ts
+++ frontend/src/app/services/session.service.ts
@@ -6,28 +6,10 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class SessionService {
-  baseQuizUrl = 'http://localhost:8080/';
-  baseLessonUrl = 'http://localhost:8081/';
-  baseActivityUrl = 'http://localhost:8082/';
-  baseSessionUrl = 'http://localhost:8083/session';
+
+  baseSessionUrl = 'http://localhost:8080/session';
 
   constructor(private http: HttpClient) { }
-
-  get_Quiz(name: string) {
-    return this.http.get(this.baseQuizUrl + name);
-  }
-
-  get_Lesson(name: string) {
-    return this.http.get(this.baseLessonUrl + name);
-  }
-
-  get_Activity(name: string) {
-    return this.http.get(this.baseActivityUrl + name);
-  }
-
-  get_Session() {
-    return this.http.get<any>(this.baseSessionUrl);
-  }
 
   getSession(sessionID: number) {
     return new Promise((resolve, reject) => {
